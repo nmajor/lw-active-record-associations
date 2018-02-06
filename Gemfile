d81dea54e405be6f3e6b868d7dc820bfa528a4f2
Nicholas Major <nick@nmajor.com>
2018-02-06 10:57:26 +0000

    Initial commuit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'pry'
+gem 'rest-client'
+gem 'faker'
+gem 'rspec'
+gem 'rake'
+gem 'activerecord'
+gem 'sqlite3'
---
+++ app/models/doctor.rb
@@ -0,0 +1,12 @@
+class Doctor < ActiveRecord::Base
+  has_many :interns
+  has_many :consultations
+  has_many(:patients, { through: :consultations })
+
+  validates(:last_name, { presence: true })
+end
+
+
+# if doctor.save
+# else
+# end
